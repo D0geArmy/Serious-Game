87406fdfb533d2254cef11d98ea790424258ec96
D0geArmy <32254536+D0geArmy@users.noreply.github.com>
2017-11-22 09:26:19 +0530

    Project file upload

---
+++ Unity Project File/Sado/Assets/NodeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour {
+
+    public int NodeNo;
+    public NodeController[] connectedNodes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
