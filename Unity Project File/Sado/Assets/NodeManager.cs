c8900748db326cba4bd6fc4a576121f21635225f
D0geArmy <32254536+D0geArmy@users.noreply.github.com>
2017-11-22 17:02:50 +0530

    Project file reupload

---
+++ Unity Project File/Sado/Assets/NodeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+
+    NodeController[] iNode;
+
+	// Use this for initialization
+	void Start () {
+        iNode = GetComponents<NodeController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
