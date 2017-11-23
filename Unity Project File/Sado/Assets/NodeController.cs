0b263e863a684812d90074f44c3376cb75e5a9cb
D0geArmy <32254536+D0geArmy@users.noreply.github.com>
2017-11-23 16:52:50 +0530

    Updated nodes manager an node controller
    work on node based objectives and node based dialogue triggers

--- Unity Project File/Sado/Assets/NodeController.cs
+++ Unity Project File/Sado/Assets/NodeController.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class NodeController : MonoBehaviour {
 
     public int NodeNo;
     public NodeController[] connectedNodes;
 
+    NodeManager myManager;
+    Transform Player;
+    NodeController currentNode;
+
 	// Use this for initialization
 	void Start () {
-		
+        myManager = FindObjectOfType<NodeManager>().gameObject.GetComponent<NodeManager>();
+        Player = myManager.player;
+        print(myManager);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
+
+    //on click move to clicked object
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //find the node player is on
+            currentNode = myManager.currentNode(NodeNo);
+            //find if th clicked node is part of the connected nodes in current node
+            foreach (NodeController nc in currentNode.connectedNodes) {
+                if (nc.NodeNo == NodeNo)
+                {
+                    Player.position = transform.position;
+                }
+            }
+        }
+    }
 }
--- Unity Project File/Sado/Assets/NodeManager.cs
+++ Unity Project File/Sado/Assets/NodeManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class NodeManager : MonoBehaviour {
 
+    [SerializeField]
     NodeController[] iNode;
+    public Transform player;
 
 	// Use this for initialization
 	void Start () {
-        iNode = GetComponents<NodeController>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    //Returns the current node the player is in
+    public NodeController currentNode(int nodeNo)
+    {
+        foreach(NodeController n in iNode)
+        {
+            Transform nt = n.gameObject.transform;
+            if(nt.position == player.position)
+            {
+                nodeNo = n.NodeNo;
+            }
+        }
+        return iNode[nodeNo];
+    }
 }
