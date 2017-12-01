6a6cbd2a3d28e2f0f94fc8a1ee95c06097ffd98a
D0geArmy <32254536+D0geArmy@users.noreply.github.com>
2017-12-01 16:52:59 +0530

    Update Proj

---
+++ Unity Project File/Sado/Assets/Scripts/MiniGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameManager : MonoBehaviour 
+{
+	//Public Interger that will control the count of all the boxes of the same color
+	public int checkforred;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
